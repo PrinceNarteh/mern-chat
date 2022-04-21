ed48059b806aafbe91ecead57bd5a3db41991236
Prince Narteh <prinarteh@gmail.com>
2022-04-21 18:14:42 +0000

    structure the chat page

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import MessageForm from "../components/MessageForm";
+import SideBar from "../components/SideBar";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="flex h-[calc(100vh_-_3.5rem)]">
+      <div className="w-4/12">
+        <SideBar />
+      </div>
+      <div className="w-8/12">
+        <MessageForm />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
