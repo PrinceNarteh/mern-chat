a1b8862d0a48523aa36d3890d74420628e07f42a
Prince Narteh <prinarteh@gmail.com>
2022-04-22 01:58:31 +0000

    adds from for senting message

--- client/src/components/MessageForm.tsx
+++ client/src/components/MessageForm.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const MessageForm = () => {
-  return <div>MessageForm</div>;
+  return (
+    <div className="flex flex-col h-[calc(100vh_-_3.5rem)]">
+      <div className="flex-grow">Message Output</div>
+      <form className="bg-slate-500">
+        <input
+          type="text"
+          className="w-11/12 border border-[#48A3D8] outline-none"
+        />
+        <button className="text-center">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default MessageForm;
--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const rooms = ["First Room", "Second Room", "Third Room", "Forth Room"];
+
+  return (
+    <div>
+      <h1 className="text-xl my-5 w-7/12 mx-auto">Available Rooms</h1>
+
+      <div className="w-7/12 mx-auto text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded  dark:border-gray-600 dark:text-white">
+        {rooms.map((room, index) => (
+          <a
+            key={index}
+            href="#"
+            aria-current="true"
+            className="block w-full px-4 py-2 text-gray-800 bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer dark:hover:bg-gray-200 dark:bg-gray-100 dark:border-gray-600"
+          >
+            {room}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
